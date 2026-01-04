e148ca4fc1bee04cb2eff20132c6600902b873a1
Alexandros Liaskos <alexliaskosga@gmail.com>
2026-01-05 01:17:54 +0200

    Remove more conflicting Radix/Next.js files

--- website/src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export { Hero } from "./Hero";
-export { Section } from "./Section";
-export { FeatureCard } from "./FeatureCard";
-export { DeadlineCard } from "./DeadlineCard";
-export { EventCalendar } from "./EventCalendar";
-export { FieldTripMap } from "./FieldTripMap";
